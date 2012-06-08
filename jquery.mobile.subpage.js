62426a930c22ddc8dae30d4443f9a910c033e654
ToddThomson <achilles@telus.net>
2012-06-08 10:43:47 -0700

    Code rewrite of version 1.1 RC

--- jquery.mobile.subpage.js
+++ jquery.mobile.subpage.js
@@ -1,4 +1,4 @@
-/*
+﻿/*
 * jQuery Mobile Framework SubPage Widget, version 1.1
 * Copyright(c) Achilles Software.
 * Authored by Todd J. Thomson, achilles@telus.net
@@ -11,9 +11,9 @@
 */
 
 /* 
-* Tested with dependent libraries:
-*    jQuery Mobile version 1.1
-*/
+ * Tested with dependent libraries:
+ *    jQuery Mobile version 1.1
+ */
 
 /*
 * Description
@@ -34,7 +34,7 @@
 
 (function ($, undefined) {
 
-    // Keeps track of the number of subpages per parent page UID
+    // Keeps track of the number of subpages per parent page UId
     var subpageCountPerPage = {};
 
     $.widget("mobile.subpage", $.mobile.widget, {
@@ -45,9 +45,11 @@
         _create: function () {
             var t = this;
 
+            // Subpages may only be defined with pages dynamically loaded via loadPage().
+            // Pages loaded this way always have a unique data-url attribute set.
             this.parentPage = this.element.closest(":jqmData(role='page')");
+            this.parentPageUrl = this.parentPage.jqmData("url");
 
-            // Create subpage markup
             t.element.addClass(function (i, orig) {
                 return orig + " ui-subpage ";
             });
@@ -56,22 +58,18 @@
         },
 
         _createSubPage: function () {
-            var self = this;
-            var subpage = this.element;
-            var subpageId = subpage.attr("id");
-            var parentPage = subpage.closest(":jqmData(role='page')");
-            var parentUrl = parentPage.jqmData("url");
-            var parentId = parentUrl || parentPage[0][$.expando];
+            var self = this,
+                subpage = this.element,
+                subpageType = subpage.jqmData("role");
 
-            if (typeof subpageCountPerPage[parentId] === "undefined") {
-                subpageCountPerPage[parentId] = -1;
+            // Subpages should have an id attribute, but we cannot guarentee this.
+            // Fallback to the subpage count with the page as the UId
+            if (typeof subpageCountPerPage[this.parentPageUrl] === "undefined") {
+                subpageCountPerPage[this.parentPageUrl] = -1;
             }
 
-            var subpageUId = subpageId || ++subpageCountPerPage[parentId];
-            var subpageId = subpage.attr("id") || subpageUId;
-            var subpageType = subpage.jqmData("role");
-            var subpageContent = subpage.find(":jqmData(role='content')");
-            var subpageUrl = (parentUrl || "") + "&" + $.mobile.subPageUrlKey + "=" + subpageUId;
+            var subpageUId = subpage.attr("id") || ++subpageCountPerPage[this.parentPageUrl];
+            var subpageUrl = this.parentPageUrl + "&" + $.mobile.subPageUrlKey + "=" + subpageUId;
 
             var newPage = subpage.detach();
 
@@ -80,6 +78,8 @@
                 .attr("data-" + $.mobile.ns + "role", 'page');
 
             // work-around for dialogs not getting default content theme of "c"
+            var subpageContent = subpage.find(":jqmData(role='content')");
+
             if (subpageType === "subpage-dialog" && subpageContent.jqmData("theme") === undefined) {
                 subpageContent.attr("data-" + $.mobile.ns + "theme", 'c');
             }
@@ -95,33 +95,30 @@
 
             // on pagehide, remove any nested pages along with the parent page, as long as they aren't active
             // and aren't embedded
-            if (parentPage.is(":jqmData(external-page='true')") &&
-                parentPage.data("page").options.domCache === false) {
+            if ( this.parentPage.is(":jqmData(external-page='true')") &&
+                 this.parentPage.data("page").options.domCache === false) {
 
-                var newRemove = function (e, ui) {
+                var subpageRemove = function (e, ui) {
                     var nextPage = ui.nextPage, npURL;
 
                     if (ui.nextPage) {
                         npURL = nextPage.jqmData("url");
-                        if (npURL.indexOf(parentUrl + "&" + $.mobile.subPageUrlKey) !== 0) {
+                        if (npURL.indexOf( self.parentPageUrl + "&" + $.mobile.subPageUrlKey) !== 0) {
                             self.childPages().remove();
-                            parentPage.remove();
+                            self.parentPage.remove();
                         }
                     }
                 };
 
                 // unbind the original page remove and replace with our specialized version
-                parentPage
+                this.parentPage
                     .unbind("pagehide.remove")
-                    .bind("pagehide.remove", newRemove);
+                    .bind("pagehide.remove", subpageRemove);
             }
         },
 
-        // TODO sort out a better way to track sub pages of the parent page
         childPages: function () {
-            var parentUrl = this.parentPage.jqmData("url");
-
-            return $(":jqmData(url^='" + parentUrl + "&" + $.mobile.subPageUrlKey + "')");
+            return $(":jqmData(url^='" + this.parentPageUrl + "&" + $.mobile.subPageUrlKey + "')");
         }
     });
 
